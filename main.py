3e82df8179ad244545ef3082f5faa7178b085b7a
Andrew Choi <choiandrew585@gmail.com>
2024-04-29 12:54:35 -0400

    Implement loss function

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
     x_train, y_train, x_test, y_test = functions.create_dataset()
 
     model = functions.create_model()
-
-    predictions = model(x_train[:5]).numpy()
+    n = 1
+    predictions = model(x_train[:n]).numpy()
     print("predictions: ", predictions)
 
     #turns the predictions into probabilities of occuring based on the predictions made
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     
     #notice how the total of the probabilities is 1 (to ~5-6 decimal point accuracy)
     total = 0
-    for i in range(5): #need 2 for loops [[probabilities[0]], probabilities[1], ... etc.]
+    for i in range(n): #need 2 for loops [[probabilities[0]], probabilities[1], ... etc.]
         for prob in probabilities[i]:
             total += prob
         print("total for probabilities[{}]: {}".format(i, total))
@@ -25,5 +25,10 @@ if __name__ == '__main__':
 
     #loss function: used to quantify the difference between predicted and real values
     loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+    #the approximate loss should be -log(1/10) ~= 2.36, 
+    #since there are ten entries in each prediction class
+    loss = loss_fn(y_train[:n], predictions).numpy()
+    print(loss)
 
     
